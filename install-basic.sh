1a00954270bb32da4079a470713e4af1272d2b2d
João Paulo Pizani Flor <joaopizani@gmail.com>
2013-12-23 21:14:20 -0200

    refactorings suggested by ShellChecker, quotings, etc.

--- install-basic.sh
+++ install-basic.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 REL_SRC=${BASH_SOURCE[0]}
-CANONICAL_SRC=$(readlink -f $REL_SRC)
+CANONICAL_SRC=$(readlink -f "$REL_SRC")
 DIR="$(cd -P "$(dirname $CANONICAL_SRC)" && pwd)"
 
 MPDDIR="${HOME}/.mpd"
 NCMPCPPDIR="${HOME}/.ncmpcpp"
 
-mkdir -p ${MPDDIR} ${NCMPCPPDIR}
+mkdir -p "${MPDDIR}" "${NCMPCPPDIR}"
 touch "${MPDDIR}/database" "${MPDDIR}/log"
 
 ln -s -f  "${DIR}/mpd.conf.local"   "${DIR}/mpd.conf"
@@ -18,6 +18,6 @@ MUSICDIR="${HOME}/Music"
 MUSICMETA="${MUSICDIR}/meta"
 MUSICLYRICS="${MUSICMETA}/lyrics"
 MUSICPLAYLISTS="${MUSICMETA}/playlists"
-mkdir -p ${MUSICLYRICS} ${MUSICPLAYLISTS}
-ln -s -f ${MUSICLYRICS}  "${HOME}/.lyrics"
+mkdir -p "${MUSICLYRICS}"  "${MUSICPLAYLISTS}"
+ln -s -f "${MUSICLYRICS}"  "${HOME}/.lyrics"
 
--- install-with-extra-steps-debian-like.sh
+++ install-with-extra-steps-debian-like.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-REL_SRC=${BASH_SOURCE[0]}
-CANONICAL_SRC=$(readlink -f $REL_SRC)
+REL_SRC="${BASH_SOURCE[0]}"
+CANONICAL_SRC=$(readlink -f "$REL_SRC")
 DIR="$(cd -P "$(dirname $CANONICAL_SRC)" && pwd)"
 
 "${DIR}/install-basic.sh"
@@ -12,7 +12,7 @@ sudo update-rc.d mpd disable
 
 # GNOME-based
 AUTOSTART_DIR="${HOME}/.config/autostart"
-mkdir -p ${AUTOSTART_DIR}
+mkdir -p "${AUTOSTART_DIR}"
 ln -s -f "${DIR}/mpd.desktop" "${AUTOSTART_DIR}/mpd.desktop"
 
 mpd
